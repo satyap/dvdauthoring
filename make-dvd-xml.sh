86acb5788545c9110c4003dbf7d51a9860cf2b0b
Satya <github@thesatya.com>
2014-09-19 21:58:47 -0700

    The make-dvd script now builds an ISO and tells you the wodim command to run

--- make-dvd-xml.sh
+++ make-dvd-xml.sh
@@ -1,8 +1,12 @@
 type=pal
+xmlfile=Dvd.xml
+xml_basicfile=Dvd-basic.xml
+
 rm t.xml
 page=0
-for x in 200*
+for x in 20*
 do cd $x
+    echo $x
     page=`expr $page + 1`
     rm -f dvdpage*
     perl ~/dvdauthoring/vid2dvd.pl -t $type -l $x/ -p $page -m ../menus/$x > t.sh
@@ -15,6 +19,16 @@ do cd $x
 done
 
 
-cat Dvd-basic.xml t.xml > Dvd.xml
+cat $xml_basicfile t.xml > $xmlfile
 rm t.xml
-echo "</dvdauthor>" >> Dvd.xml
+echo "</dvdauthor>" >> $xmlfile
+(cd rootmenu && make ${type} ${type}video)
+
+dvdauthor -x $xmlfile
+genisoimage -dvd-video -o ${type}dvd.iso dvdfs
+
+echo
+echo Commands to run next: 
+echo wodim -v speed=1 ${type}dvd.iso
+echo
+
