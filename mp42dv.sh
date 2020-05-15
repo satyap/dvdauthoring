9e1d2f6e0615ac1c94a4838f914e516ca055bf93
Satya <github@thesatya.com>
2020-05-14 22:30:23 -0700

    Turn off auto-rotation

--- mp42dv.sh
+++ mp42dv.sh
@@ -2,5 +2,5 @@
 
 echo -n "\033]0; Converting $1\007"
 #/usr/bin/avconv -y -i "$1" -target ntsc-dv -aspect 4:3 -f dv "$1.dv"
-/usr/bin/ffmpeg -y -i "$1" -target ntsc-dv -aspect 4:3 -f dv "$1.dv"
+/usr/bin/ffmpeg -y -noautorotate -i "$1" -target ntsc-dv -aspect 4:3 -f dv "$1.dv"
 
